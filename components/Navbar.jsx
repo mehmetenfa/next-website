b4795b09467e3116739cb8eb4d60e0f7d2bcfdd6
qxan <mehmet.enfa@gmail.com>
2022-12-30 15:23:54 +0300

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,27 +1,50 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300">
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/">
-          <h1>Capture</h1>
+          <h1 className="font-bold text-4xl">Capture</h1>
+        </Link>
+        <ul className="hidden sm:flex">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/#gallery">Gallery</Link>
+          </li>
+          <li>
+            <Link href="/portfolio">Work</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        {/* Mobile Button */}
+        <div>
+          <AiOutlineMenu size={20} />
+        </div>
+        {/* Mobile Menu */}
+        <div>
           <ul>
             <li>
-                <Link href='/'>Home</Link>
+              <Link href="/">Home</Link>
             </li>
             <li>
-                <Link href='/#gallery'>Gallery</Link>
+              <Link href="/#gallery">Gallery</Link>
             </li>
             <li>
-                <Link href='/portfolio'>Work</Link>
+              <Link href="/portfolio">Work</Link>
             </li>
             <li>
-                <Link href='/contact'>Contact</Link>
+              <Link href="/contact">Contact</Link>
             </li>
           </ul>
-        </Link>
+        </div>
       </div>
     </div>
   );
