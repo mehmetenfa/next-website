42453d71f505cfa30d5c07e7b3d7348757f3e9e2
qxan <mehmet.enfa@gmail.com>
2022-12-30 15:28:49 +0300

    navbar mobile responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,26 +10,26 @@ const Navbar = () => {
           <h1 className="font-bold text-4xl">Capture</h1>
         </Link>
         <ul className="hidden sm:flex">
-          <li>
+          <li className="p-4">
             <Link href="/">Home</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/#gallery">Gallery</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/portfolio">Work</Link>
           </li>
-          <li>
+          <li className="p-4">
             <Link href="/contact">Contact</Link>
           </li>
         </ul>
 
         {/* Mobile Button */}
-        <div>
+        <div className="block sm:hidden z-10">
           <AiOutlineMenu size={20} />
         </div>
         {/* Mobile Menu */}
-        <div>
+        <div className="sm:hidden absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300">
           <ul>
             <li>
               <Link href="/">Home</Link>
