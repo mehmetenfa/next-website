1e865c28bf4cec0dcdde44f6602c7e58fd34febe
qxan <mehmet.enfa@gmail.com>
2022-12-30 15:15:15 +0300

    navbar => app.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <div>
-        
+      <div>
+        <Link href="/">
+          <h1>Capture</h1>
+          <ul>
+            <li>
+                <Link href='/'>Home</Link>
+            </li>
+            <li>
+                <Link href='/#gallery'>Gallery</Link>
+            </li>
+            <li>
+                <Link href='/portfolio'>Work</Link>
+            </li>
+            <li>
+                <Link href='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
